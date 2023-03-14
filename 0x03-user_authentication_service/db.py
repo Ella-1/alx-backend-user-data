9d09b35b3c78d88c3e7c5b9952fcdca51cbcc106
Ella-1 <es9897288@gmail.com>
2023-03-14 04:02:47 -0700

    task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
         self._session.commit()
         return user
 
-     def find_user_by(self, **kwargs) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """ takes in arbitrary keyword arguments and returns the first row
             found in the users table as filtered by the method’s input
             arguments """
