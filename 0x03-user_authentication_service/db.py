465ecad6098f6ef656561e2cf873066286fa8e98
Ella-1 <es9897288@gmail.com>
2023-03-14 03:55:48 -0700

    task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,3 +32,14 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+     def find_user_by(self, **kwargs) -> User:
+        """ takes in arbitrary keyword arguments and returns the first row
+            found in the users table as filtered by the method’s input
+            arguments """
+        if kwargs is None:
+            raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
